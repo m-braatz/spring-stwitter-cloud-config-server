deb06f917321e9c5f17c26bd7c1880237b48e780
m-braatz <msbraatz@gmail.com>
2019-09-03 09:59:44 -0500

    First commit

---
+++ src/main/java/com/company/springstwittercloudconfigserver/SpringStwitterCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.springstwittercloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStwitterCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStwitterCloudConfigServerApplication.class, args);
+	}
+
+}
